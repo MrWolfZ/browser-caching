29b3ad2a85392a1e1e35ff22e8827a41c7169ff3
MrWolfZ <ziller.jonathan@web.de>
2015-05-12 15:50:04 +0200

    fixed export issues in index.js

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,18 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./cacheFactory'));
-__export(require('./cache'));
+var abstractCache_1 = require('./abstractCache');
+exports.AbstractCache = abstractCache_1.default;
+var cacheFactory_1 = require('./cacheFactory');
+exports.CacheFactory = cacheFactory_1.default;
+var cacheItem_1 = require('./cacheItem');
+exports.CacheItem = cacheItem_1.default;
 __export(require('./cachingMode'));
-__export(require('./evictionPolicy'));
+var evictionPolicy_1 = require('./evictionPolicy');
+exports.EvictionPolicy = evictionPolicy_1.EvictionPolicy;
+var infiniteCache_1 = require('./infiniteCache');
+exports.InfiniteCache = infiniteCache_1.default;
+var sessionCache_1 = require('./sessionCache');
+exports.SessionCache = sessionCache_1.default;
+var transientCache_1 = require('./transientCache');
+exports.TransientCache = transientCache_1.default;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export * from './cacheFactory';
-export * from './cache';
+export { default as AbstractCache } from './abstractCache';
+export { ICache } from './cache';
+export { default as CacheFactory } from './cacheFactory';
+export { default as CacheItem } from './cacheItem';
 export * from './cachingMode';
-export * from './evictionPolicy';
+export { EvictionPolicy } from './evictionPolicy';
+export { default as InfiniteCache } from './infiniteCache';
+export { default as SessionCache } from './sessionCache';
+export { default as TransientCache } from './transientCache';
